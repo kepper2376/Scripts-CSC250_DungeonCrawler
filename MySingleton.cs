00bbb51000dbf1b59b55c919cb7368157ce7c0ca
kepper2376 <matthew.koepke@cuw.edu>
2024-02-15 21:32:58 -0500

    HW #11 - in class 3/13

--- MySingleton.cs
+++ MySingleton.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class MySingleton // removed monobehavior removes start and update
 {
-    static public int secretNumber = -1; // static owned by the class - to access outside of the class with the name of the class
-
+     // static owned by the class - to access outside of the class with the name of the class
+    public static string currentDirection = "?";
 }
--- Player.cs
+++ Player.cs
@@ -4,29 +4,43 @@ using UnityEngine;
 using UnityEditor.SceneManagement;
 
 
-public class Player1 : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    private float speed = 2.0f; 
-    public GameObject destinationGO;   
+    private float speed = 2.0f; // f at the end bc its a float rather than a double
+    // double can hold bigger values with more precision compared to floats
+    public GameObject northExit;  
+    public GameObject southExit; 
+    public GameObject eastExit; 
+    public GameObject westExit; 
+    
 
     // Start is called before the first frame update
     void Start()
     {
-
+       // if not equal to 
+       // if not our first scene
+       if(!MySingleton.currentDirection.Equals("?")) // checks to see the contents of the string the same rather than == would be for pointers to be the same
+       {
+            if(MySingleton.currentDirection.Equals("North"))
+            {
+                this.gameObject.transform.position = this.southExit.transform.position;
+            }
+       }
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        EditorSceneManager.LoadScene("Scene2");
-        this.gameObject.transform.position = Vector3.Lerp(this.gameObject.transform.position, this.destinationGO.transform.position, this.speed * Time.deltaTime);
-    }
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.CompareTag("Door"))
+            {
+            MySingleton.currentDirection = "North";
+            EditorSceneManager.LoadScene("DungeonRoom");
+            }             
+         }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(this.gameObject.transform.position, this.destinationGO.transform.position) > 1.0)
-        {
-            this.gameObject.transform.position = Vector3.Lerp(this.gameObject.transform.position, this.destinationGO.transform.position, this.speed * Time.deltaTime);
-        }
+       // MoveTowards wants us to give it (current location, destination we want it to go to, )
+        this.gameObject.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.northExit.transform.position, this.speed * Time.deltaTime);
     }
 }
